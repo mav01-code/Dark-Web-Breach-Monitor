4f3d39efb8aa9e55a05671478a8d861df8782a1e
mav01-code <marreddyakshayavarshini@gmail.com>
2026-02-27 12:19:27 +0530

    Defined methods for routes

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from flask import Flask, render_templates, request, redirect, session, url_for
-import bycrypt
+from flask import Flask, render_template, request, redirect, session, url_for
+import bcrypt
 import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = "password"
 
 def get_connection():
-    return mysql.connector.connect(host = "localhost", user = "root", password = "Varsini@102006", database = "MONITOR")
+    return mysql.connector.connect(host = "localhost", user = "root", password = "", database = "MONITOR")
 
+@app.route("/register", methods = ["POST"])
 def register():
     pass
 
+@app.route("/login", methods = ["GET"])
 def login():
     pass
 
-def dashboard():
+@app.route("/credentials", methods = ["POST"])
+def credentials():
     pass
 
+@app.route("/check", methods = ["GET"])
+def check():
+    pass
+
+@app.route("/logout")
 def logout():
     pass
 
